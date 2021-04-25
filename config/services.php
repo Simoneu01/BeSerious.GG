474fbca1f37afbdae1a79133a783b65a565e22b9
Simoneu01 <simoneungaro@hotmail.it>
2021-04-25 15:39:53 +0200

    Enable login with Github and Google

--- config/services.php
+++ config/services.php
@@ -35,4 +35,15 @@ return [
         'client_secret' => env('GAMESHARD_CLIENT_SECRET'),
         'redirect' => env('GAMESHARD_REDIRECT_URL'),
     ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
+    ],
+      'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+    ],
 ];
