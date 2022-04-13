f3ca9991dddefbfc5d012e204156deca9b40486f
Matteo Besutti <matteo.besutti@virgilio.it>
2022-04-13 23:43:12 +0200

    start frontend branch

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -155,11 +155,11 @@
         <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
             <div class="text-center">
                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                    <span class="block xl:inline">6ix</span>
-                    <span class="block text-red-600 xl:inline">Project</span>
+                    <span class="block xl:inline">Be</span>
+                    <span class="block text-red-600 xl:inline">Serious</span>
                 </h1>
                 <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                    La community italiana di Rainbow Six Siege
+                    Il campionato cadetto Italiano di Rainbow Six: Siege
                 </p>
                 <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
                     <div class="rounded-md shadow">
