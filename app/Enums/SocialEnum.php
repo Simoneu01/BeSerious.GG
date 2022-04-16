c1d20f84f9698381b850a090e0d14967a036a258
Simone <simoneungaro@hotmail.it>
2022-04-16 14:21:31 +0200

    Add classes to social enum

--- app/Enums/SocialEnum.php
+++ app/Enums/SocialEnum.php
@@ -22,6 +22,18 @@ enum SocialEnum: string
         };
     }
 
+    public function getClasses(): string
+    {
+        return match ($this) {
+            SocialEnum::TWITTER => 'hover:text-[#00acee]',
+            SocialEnum::LINKEDIN => 'hover:text-[#0e76a8]',
+            SocialEnum::INSTAGRAM => 'hover:text-orange-500',
+            SocialEnum::FACEBOOK => 'hover:text-[#1778F2]',
+            SocialEnum::TWITCH => 'hover:text-[#9147ff]',
+            default => 'hover:text-gray-500'
+        };
+    }
+
     public function getLabel(): string
     {
         return ucfirst($this->value);
--- resources/views/components/staff-member.blade.php
+++ resources/views/components/staff-member.blade.php
@@ -8,14 +8,15 @@
     <div class="space-y-2">
         <div class="text-lg leading-6 font-medium space-y-1">
             <h3>{{ $member->full_name }}</h3>
-            <p class="text-indigo-600">{{ $member->role }}</p>
+            <p class="text-red-600">{{ $member->role }}</p>
         </div>
         <ul class="flex space-x-5">
             @foreach($member->socials as $social)
+                <?php /** @var \App\Enums\SocialEnum $social */?>
                 <li>
                     <a href="{{ $social->url }}" class="text-gray-400 hover:text-gray-500">
                         <span class="sr-only">{{ $social->type->getLabel() }}</span>
-                        <x-dynamic-component :component="$social->type->getIcon()" class="h-5 w-5" />
+                        <x-dynamic-component :component="$social->type->getIcon()" class="h-5 w-5 {{ $social->type->getClasses() }}" />
                     </a>
                 </li>
             @endforeach
