bbfc40c9ffcbbc206f42ce5270ab716daf26ed1f
Simone <simoneungaro@hotmail.it>
2022-05-05 21:21:16 +0200

    Create 2022_04_29_212358_create_games_table.php

---
+++ database/migrations/2022_04_29_212358_create_games_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('gameshard_match_id');
+            $table->foreignId('home_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->foreignId('away_team_id')->constrained('teams')->cascadeOnDelete();
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
