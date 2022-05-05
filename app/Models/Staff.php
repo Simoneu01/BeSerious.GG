751574394e46dd49c1a9714978f423ae25d6de9b
Simone <simoneungaro@hotmail.it>
2022-05-05 22:21:11 +0200

    Add phpdocs

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,10 +3,38 @@
 namespace App\Models;
 
 use App\Casts\Socials;
+use App\DTO\Social;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * App\Models\Staff
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $surname
+ * @property string $role
+ * @property string $img
+ * @property array<int, Social>|null $socials
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string $full_name
+ * @property-read string $img_url
+ * @method static \Database\Factories\StaffFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereImg($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereSocials($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereSurname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Staff extends Model
 {
     use HasFactory;
