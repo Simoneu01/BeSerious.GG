e28963b2c67aeb68e07f8aad5c16342e3e60414b
Simone Ungaro <simoneungaro@hotmail.it>
2023-01-27 11:03:21 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         "./vendor/laravel/jetstream/**/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
+        './vendor/filament/**/*.blade.php',
     ],
 
     theme: {
