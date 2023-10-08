00a5f59cfcf76229320985945962f2d06b2ba50f
Simone <simoneungaro@hotmail.it>
2023-10-08 11:29:55 +0200

    feat: add laravel-ide-helper (#56)
    * Install laravel ide helper
    
    * Add script
    
    * Run laravel ide helper
    
    * chore: styling

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -7,6 +7,35 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
+/**
+ * App\Models\Staff
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $surname
+ * @property string $role
+ * @property string $img
+ * @property array<int, Social>|null $socials
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string $full_name
+ * @property-read string $img_url
+ *
+ * @method static \Database\Factories\StaffFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereImg($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereSocials($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereSurname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class Staff extends Model
 {
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,57 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
+ * @property int|null $current_team_id
+ * @property int|null $current_connected_account_id
+ * @property string|null $profile_photo_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $two_factor_secret
+ * @property string|null $two_factor_recovery_codes
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\ConnectedAccount> $connectedAccounts
+ * @property-read int|null $connected_accounts_count
+ * @property-read \App\Models\ConnectedAccount|null $currentConnectedAccount
+ * @property-read string $profile_photo_url
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
+ * @property-read int|null $roles_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ *
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User permission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCurrentConnectedAccountId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCurrentTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProfilePhotoPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements FilamentUser, HasAvatar
 {
     use HasApiTokens;
