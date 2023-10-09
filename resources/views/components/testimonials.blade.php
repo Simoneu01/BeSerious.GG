516da3e9eef9d4872c6db1712c802a37823fbb0d
Simoneu01 <Simoneu01@users.noreply.github.com>
2023-10-09 13:39:48 +0000

    chore: styling

--- resources/views/components/testimonials.blade.php
+++ resources/views/components/testimonials.blade.php
@@ -1,17 +1,21 @@
 <section {{ $attributes->class(['bg-red-600']) }}>
     <div class="mx-auto max-w-7xl md:grid md:grid-cols-2 md:px-6 lg:px-8">
-        <div class="py-12 px-6 md:flex md:flex-col md:border-r md:border-red-500 md:py-16 md:pl-0 md:pr-10 lg:pr-16">
+        <div class="px-6 py-12 md:flex md:flex-col md:border-r md:border-red-500 md:py-16 md:pl-0 md:pr-10 lg:pr-16">
             <blockquote class="mt-6 md:flex md:flex-grow md:flex-col">
                 <div class="relative text-lg font-medium text-white md:flex-grow">
-                    <svg class="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-white opacity-25" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
-                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                    <svg class="absolute left-0 top-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-white opacity-25"
+                        aria-hidden="true" fill="currentColor" viewBox="0 0 32 32">
+                        <path
+                            d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                     </svg>
-                    <p class="relative">Seguo il progetto dal lontano 2017 quando ancora si chiamava 6ixProject, sono entusiasta di come sta crescendo e del risultato attuale, molto deve ancora arrivare!</p>
+                    <p class="relative">Seguo il progetto dal lontano 2017 quando ancora si chiamava 6ixProject, sono
+                        entusiasta di come sta crescendo e del risultato attuale, molto deve ancora arrivare!</p>
                 </div>
                 <footer class="mt-8">
                     <div class="flex items-start">
                         <div class="inline-flex flex-shrink-0 rounded-full border-2 border-white">
-                            <img class="h-12 w-12 rounded-full" src="{{ Vite::asset('resources/img/simone.jpg') }}" alt="simone-photo">
+                            <img class="h-12 w-12 rounded-full" src="{{ Vite::asset('resources/img/simone.jpg') }}"
+                                alt="simone-photo">
                         </div>
                         <div class="ml-4">
                             <div class="text-base font-medium text-white">Simone 'Simo01' Ungaro</div>
@@ -21,19 +25,25 @@
                 </footer>
             </blockquote>
         </div>
-        <div class="border-t-2 border-red-500 py-12 px-6 md:border-t-0 md:border-l md:py-16 md:pr-0 md:pl-10 lg:pl-16">
+        <div class="border-t-2 border-red-500 px-6 py-12 md:border-l md:border-t-0 md:py-16 md:pl-10 md:pr-0 lg:pl-16">
             <blockquote class="mt-6 md:flex md:flex-grow md:flex-col">
                 <div class="relative text-lg font-medium text-white md:flex-grow">
-                    <svg class="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-white opacity-25" fill="currentColor" viewBox="0 0 32 32">
-                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                    <svg class="absolute left-0 top-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-white opacity-25"
+                        fill="currentColor" viewBox="0 0 32 32">
+                        <path
+                            d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                     </svg>
-                    <p class="relative">L'idea di BeSerious è nata nel 2019/2020, in due anni siamo riusciti a raggiungere degli obiettivi non da poco, tra cui l'essere la Seconda divisione di Rainbow Six Siege ufficiale in Italia.
-                        <br/>Siamo fieri del lavoro che stiamo facendo e non siamo intenzionati a fermarci!</p>
+                    <p class="relative">L'idea di BeSerious è nata nel 2019/2020, in due anni siamo riusciti a
+                        raggiungere degli obiettivi non da poco, tra cui l'essere la Seconda divisione di Rainbow Six
+                        Siege ufficiale in Italia.
+                        <br />Siamo fieri del lavoro che stiamo facendo e non siamo intenzionati a fermarci!
+                    </p>
                 </div>
                 <footer class="mt-8">
                     <div class="flex items-start">
                         <div class="inline-flex flex-shrink-0 rounded-full border-2 border-white">
-                            <img class="h-12 w-12 rounded-full" src="{{ Vite::asset('resources/img/matteo.jpg') }}" alt="forever-photo">
+                            <img class="h-12 w-12 rounded-full" src="{{ Vite::asset('resources/img/matteo.jpg') }}"
+                                alt="forever-photo">
                         </div>
                         <div class="ml-4">
                             <div class="text-base font-medium text-white">Matteo 'Forever.exe' Besutti</div>
