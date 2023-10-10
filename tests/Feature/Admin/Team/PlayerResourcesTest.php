3c089152eef5cb33541f06f9aa43b7274b1e675f
Simone <simoneungaro@hotmail.it>
2023-10-10 12:15:02 +0200

    add filament tests

--- tests/Feature/Admin/Team/PlayerResourcesTest.php
+++ tests/Feature/Admin/Team/PlayerResourcesTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature\Admin\Team;
 
 use App\Filament\Resources\Teams\PlayerResource;
 use App\Models\Player;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class PlayerResourcesTest extends TestCase
@@ -34,5 +36,19 @@ class PlayerResourcesTest extends TestCase
         $this->get(PlayerResource::getUrl('edit', [
             'record' => Player::factory()->create(),
         ]))->assertSuccessful();
+    }
+
+    public function test_can_render_relation_manager()
+    {
+
+        $player = Player::factory()
+            ->has(Team::factory()->count(10))
+            ->create();
+
+        Livewire::test(PlayerResource\RelationManagers\TeamsRelationManager::class, [
+            'ownerRecord' => $player,
+            'pageClass' => PlayerResource\Pages\EditPlayer::class,
+        ])
+            ->assertSuccessful();
     }
 }
--- tests/Feature/Admin/Team/TeamResourcesTest.php
+++ tests/Feature/Admin/Team/TeamResourcesTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature\Admin\Team;
 
 use App\Filament\Resources\Teams\TeamResource;
+use App\Models\Player;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class TeamResourcesTest extends TestCase
@@ -34,5 +36,19 @@ class TeamResourcesTest extends TestCase
         $this->get(TeamResource::getUrl('edit', [
             'record' => Team::factory()->create(),
         ]))->assertSuccessful();
+    }
+
+    public function test_can_render_relation_manager()
+    {
+
+        $team = Team::factory()
+            ->has(Player::factory()->count(10))
+            ->create();
+
+        Livewire::test(TeamResource\RelationManagers\PlayersRelationManager::class, [
+            'ownerRecord' => $team,
+            'pageClass' => TeamResource\Pages\EditTeam::class,
+        ])
+            ->assertSuccessful();
     }
 }
